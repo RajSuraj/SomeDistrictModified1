215a66a6539be94f1225f31dae0d223bee8d22a8
RajSuraj <suraj.raj@accionlabs.com>
2013-11-07 18:52:08 +0530

    commit

---
+++ SomeDistrictModified1/SomeDistrictModified1.feature
@@ -0,0 +1,11 @@
+﻿Feature: SomeDistrictModified1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Some DistrictModified
+   Given the alteryx service is running at "http://gallery.alteryx.com"
+   When I invoke GET at "api/Districts" for "Communications District"
+   Then I see that district description contains the text "Need to establish B2B potential by geographic territory"
+
---
+++ SomeDistrictModified1/SomeDistrictModified1Steps.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SomeDistrictModified1
+{
+    [Binding]
+    public class SomeDistrictModified1Steps
+    {
+        private string _theUrl;
+        private string _theResponse;
+        private string _theapiresponse;
+
+        [Given(@"the alteryx service is running at ""(.*)""")]
+        public void GivenTheAlteryxServiceIsRunningAt(string p0)
+        {
+            _theUrl = p0;
+        }
+        
+        [When(@"I invoke GET at ""(.*)"" for ""(.*)""")]
+        public void WhenIInvokeGETAtFor(string p0, string expecteddistrict)
+        {
+            string fullUrl = _theUrl + "/" + p0;
+            WebRequest request = WebRequest.Create(fullUrl);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            _theResponse = responseFromServer;
+
+            var DistrictObj = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<dynamic>(_theResponse);
+            int count = DistrictObj.Length;
+
+            int i = 0;
+            for (i = 0; i <= count - 1; i++)
+            {
+                if (DistrictObj[i]["title"] == expecteddistrict)
+                {
+                    break;
+                }
+            }
+
+            string id = DistrictObj[i]["id"];
+            string apiUrl = fullUrl + "/" + id;
+           
+            WebRequest apirequest = WebRequest.Create(apiUrl);
+            apirequest.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse apiresponse = (HttpWebResponse)apirequest.GetResponse();
+            Stream apidataStream = apiresponse.GetResponseStream();
+            StreamReader apireader = new StreamReader(apidataStream);
+            string apiresponseFromServer = apireader.ReadToEnd();
+            _theapiresponse = apiresponseFromServer;
+
+        }
+        
+        [Then(@"I see that district description contains the text ""(.*)""")]
+        public void ThenISeeThatDistrictDescriptionContainsTheText(string expecteddescription)
+        {
+            var dict = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Dictionary<string, dynamic>>(_theapiresponse);
+
+            Console.Write(dict["description"]);
+            StringAssert.Contains(expecteddescription, dict["description"]);
+
+        }
+    }
+}
